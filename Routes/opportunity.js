7b6e365daa5bb440961a462729cb25e722d4bad7
ahmed4549 <m.ahmedanis99@gmail.com>
2023-06-05 04:10:46 +0500

    added delete opportunity api

--- Routes/opportunity.js
+++ Routes/opportunity.js
@@ -1,5 +1,7 @@
-const Opportunity = require("../Models/Opportunity");
 const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const Opportunity = require("../Models/Opportunity");
 
 router.post("/create", async (req, res) => {
   try {
@@ -22,8 +24,25 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {});
 router.put("/:id", (req, res) => {});
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+  // todo: add a auth check
   const { id } = req.params;
+  // Check if the provided ID is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid ObjectId" });
+  }
+  try {
+    const deletedDoc = await Opportunity.findByIdAndDelete(id);
+
+    if (!deletedDoc) {
+      return res.status(404).json({ error: "Document not found" });
+    }
+
+    res.json({ message: "Document deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting document:", error);
+    res.status(500).json({ message: "Server error", error });
+  }
 });
 
 module.exports = router;
